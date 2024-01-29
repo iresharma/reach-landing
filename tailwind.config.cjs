330b8b183f91b10449070bf7f9a77b7103ae4e44
iresharma <iresh.sharma8@gmail.com>
2024-01-29 23:36:33 +0530

    working on bento grids

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,20 @@ module.exports = {
 	darkMode: "class",
 	theme: {
 		extend: {
+			animation: {
+				"meteor-effect": "meteor 5s linear infinite",
+			},
+			keyframes: {
+
+				meteor: {
+					"0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+					"70%": { opacity: 1 },
+					"100%": {
+						transform: "rotate(215deg) translateX(-500px)",
+						opacity: 0,
+					},
+				},
+			},
 			colors: {
 				body: "rgb(var(--color-bg))",
 				"box-bg": "rgb(var(--color-box))",
